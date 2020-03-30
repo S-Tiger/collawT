5d46d842bec838c75894404038b4c2fb01ee4ec3
KSH <tjdghek98@naver.com>
2020-03-30 17:48:22 +0900

    프로젝트시작

---
+++ src/main/java/home/controller/HomeController.java
@@ -0,0 +1,26 @@
+package home.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		return "index";
+	}
+	
+}
