e877769a60b7292bafe0dab9d148e8c1d4f3b6ad
CEA <coa991220@gmail.com>
2020-04-01 14:34:56 +0900

    11

--- src/main/java/project/issue/controller/issueController.java
+++ src/main/java/project/issue/controller/issueController.java
@@ -4,5 +4,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface issueController {
+	
 	public String searchList() throws Exception;
 }
