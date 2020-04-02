0e1b4bfd3d9f8709aaa24fe87d63345eb70bdce1
Han <Han@LAPTOP-BPK3ED2S>
2020-04-02 16:26:35 +0900

    notify 수정

--- src/main/java/project/notify/dao/NotifyDAOImpl.java
+++ src/main/java/project/notify/dao/NotifyDAOImpl.java
@@ -2,10 +2,11 @@ package project.notify.dao;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import project.notify.vo.NotifyVO;
 
-
+@Repository
 public class NotifyDAOImpl implements NotifyDAO {
 
 	@Autowired
--- src/main/java/project/notify/service/NotifyServiceImpl.java
+++ src/main/java/project/notify/service/NotifyServiceImpl.java
@@ -1,10 +1,12 @@
 package project.notify.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import project.notify.dao.NotifyDAOImpl;
 import project.notify.vo.NotifyVO;
 
+@Service
 public class NotifyServiceImpl implements NotifyService{
 	
 	@Autowired
