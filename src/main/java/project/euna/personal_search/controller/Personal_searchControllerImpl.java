6f8d1a0c9d8de3a1d35fc7d7afd13a24b31d830b
CEA <coa991220@gmail.com>
2020-05-02 18:40:38 +0900

    (은아) ck에디터 재수정(라이브러리는 못함) / 전체 파일함 수정(시간 나면 레이아웃 다시 변경 예정)

--- src/main/java/project/euna/personal_search/controller/Personal_searchControllerImpl.java
+++ src/main/java/project/euna/personal_search/controller/Personal_searchControllerImpl.java
@@ -14,13 +14,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-
+import project.euna.issue.service.IssueServiceImpl;
 import project.euna.personal_search.service.Personal_searchService;
 import project.euna.personal_search.vo.Criteria;
 import project.euna.personal_search.vo.PageMaker;
+import project.euna.whole_search.service.Whole_searchService;
+import project.euna.whole_search.vo.fileCriteria;
+import project.euna.whole_search.vo.filePageMaker;
 
 @Controller
 
@@ -30,6 +33,12 @@ public class Personal_searchControllerImpl implements Personal_searchController 
 	//
 	@Inject
 	Personal_searchService personal_searchService;
+	
+	@Inject
+	Whole_searchService whole_searchService;
+	
+	@Inject
+	IssueServiceImpl issueService;
 
 	
 	//글 목록 조회 페이징
@@ -74,18 +83,49 @@ public class Personal_searchControllerImpl implements Personal_searchController 
 	//파일함
 	@Override
 	@GetMapping("/myFile")
-	public ModelAndView myFile(HttpSession session, HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public ModelAndView myFile(
+			@RequestParam(required=false) String keyword,
+			@RequestParam(required=false) String c_Id,
+			@RequestParam(required=false) String order,
+			@RequestParam(required=false) String a_NameEx,
+			@RequestParam(required=false) String wr_mem_Id,
+			fileCriteria filecri, HttpSession session, HttpServletRequest request, 
+			HttpServletResponse response) throws Exception {
 		
 		Map<String, Object> member = new HashMap<String,Object>();
 		member = (Map<String, Object>) session.getAttribute("member");
 		String mem_Id = (String) member.get("mem_Id");
-				
-		List<Map> list = personal_searchService.myFile(mem_Id);
 		
 		
-		ModelAndView mav = new ModelAndView("/personal/myFile");
-		mav.addObject("myFile", list);
+		filecri.setKeyword(keyword);
+		filecri.setMem_Id(mem_Id);
 		
+		filePageMaker filepageMaker = new filePageMaker();
+		filepageMaker.setCri(filecri);
+		filepageMaker.setTotalCount(whole_searchService.fileCount(filecri));
+		
+		List<Map> fileList = whole_searchService.searchFile(filecri);
+		
+		int fileCount = whole_searchService.fileCount(filecri);
+		
+		List<Map> coworkList = issueService.coRead(mem_Id);
+		
+		List<Map> fileWriter = whole_searchService.fileWriter(filecri);
+		
+		List<Map> fileEx = whole_searchService.fileEx(filecri);
+	
+		ModelAndView mav = new ModelAndView("personal/myFile");
+		mav.addObject("filepageMaker", filepageMaker);
+		mav.addObject("fileList", fileList);
+		mav.addObject("fileCount", fileCount);
+		mav.addObject("coworkList", coworkList);
+		mav.addObject("fileWriter", fileWriter);
+		mav.addObject("fileEx", fileEx);
+		mav.addObject("keyword", keyword);
+		mav.addObject("c_Id", c_Id);
+		mav.addObject("order", order);
+		mav.addObject("wr_mem_Id", wr_mem_Id);
+		mav.addObject("a_NameEx", a_NameEx);
 		return mav;
 		
 	}
